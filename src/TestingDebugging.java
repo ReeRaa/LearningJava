b9097fdf3f04f4ff6998e19a637d37faf46b17cb
RRaa <35782666+RRaa@users.noreply.github.com>
2018-08-27 09:54:31 +0300

    Testing Debugging
    Signed-off-by: RRaa <35782666+RRaa@users.noreply.github.com>

--- src/TestingDebugging.java
+++ src/TestingDebugging.java
@@ -2,14 +2,20 @@
 public class TestingDebugging {
     public static void main(String args[]){
         int i,k;
+        //alt üles
         for (i=0;i<9;i++){
             System.out.println();
             for (k=i;k<9;k++){
                 System.out.print("*");
             }
         }
+
+        //Ülevalt alla
+        for (i=9;i>0;i--){
+            System.out.println();
+            for (k=i;k<9;k++){
+                System.out.print("*");
+            }
+        }
     }
-int i;
-
-
 }
