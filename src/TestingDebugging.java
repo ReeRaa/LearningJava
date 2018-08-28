000cd0e0162b1fb43cbb27a6d9894ba13590c17a
RRaa <35782666+RRaa@users.noreply.github.com>
2018-08-28 11:09:44 +0300

    Testing Debugging
    Signed-off-by: RRaa <35782666+RRaa@users.noreply.github.com>

--- src/TestingDebugging.java
+++ src/TestingDebugging.java
@@ -1,21 +1,38 @@
 
 public class TestingDebugging {
-    public static void main(String args[]){
-        int i,k;
+    public static void main(String args[]) {
+        /*int i, k;
         //alt üles
-        for (i=0;i<9;i++){
+        for (i = 0; i < 9; i++) {
             System.out.println();
-            for (k=i;k<9;k++){
+            for (k = i; k < 9; k++) {
                 System.out.print("*");
             }
         }
 
         //Ülevalt alla
-        for (i=9;i>0;i--){
+        for (i = 9; i > 0; i--) {
             System.out.println();
-            for (k=i;k<9;k++){
+            for (k = i; k < 9; k++) {
                 System.out.print("*");
             }
         }
+        */
+        System.out.println();
+        ParameetrigaMeetod param1 = new ParameetrigaMeetod();
+        ParameetrigaMeetod param2 = new ParameetrigaMeetod();
+        ParameetrigaMeetod param3 = new ParameetrigaMeetod();
+
+        param1.width=22;
+        param1.length=3;
+        param2.width=22;
+        param2.length=4;
+        System.out.println("Area  = " + param1.calculatedArea());
+        if (param1.calculatedArea()>param2.calculatedArea()) System.out.println("Area1 is bigger than Area 2");
+        else System.out.println("Area2 is bigger than Area 1");
+
+        //parameter casting
+        System.out.println("Area  = " + param3.calcParam(11,4));
+
     }
 }
