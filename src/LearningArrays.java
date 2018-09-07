e59531dd9f41d72bc0ce30f03e658caa5cbd2cc8
RRaa <35782666+RRaa@users.noreply.github.com>
2018-09-07 18:58:10 +0300

    Final sorting
    Signed-off-by: RRaa <35782666+RRaa@users.noreply.github.com>

--- src/LearningArrays.java
+++ src/LearningArrays.java
@@ -6,7 +6,9 @@ public class LearningArrays {
             salaries[i]=i;
             System.out.println(salaries[i]);
         }
+       //find min and max values
         int maxMin[] = {2,4,5,7,1};
+        int sortedArray[]=new int [5];
         int min, max;
         min=max=maxMin[0];
         for (i=1;i<5;i++){
@@ -14,5 +16,20 @@ public class LearningArrays {
             if ((maxMin[i])>max) max=maxMin[i];
         }
         System.out.println("Max = "+ max + " Min = "+ min);
+
+        //find, on which place is letter A
+        char someChar[]={'B','C','D','A','F','A'};
+        char right ='A';
+        for (i=0;i<6;i++){
+            if (someChar[i]==right) System.out.println("Letter A is on place "+ (i+1));
+        }
+
+        //find where is word 'mets'
+        String sonad[]={"puud","koobas","mets","linnud"};
+        String rightWord="mets";
+        for (i=0;i<4;i++){
+            if (sonad[i]==rightWord) System.out.println("Searched word is '" + rightWord + "' which is on place "+ (i+1));
+        }
+
     }
 }
