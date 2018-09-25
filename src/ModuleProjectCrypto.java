ae0de638f441baef0c1ccd3bca88ad8e1a69d401
RRaa <35782666+RRaa@users.noreply.github.com>
2018-09-25 08:01:16 +0300

    1. String normalization method done and tested. 2. Second method for adding 'OB'language part done and tested
    Signed-off-by: RRaa <35782666+RRaa@users.noreply.github.com>

--- src/ModuleProjectCrypto.java
+++ src/ModuleProjectCrypto.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class ModuleProjectCrypto {
     public static void main(String[] args){
-        String word=" ti4!mbu!        *')  lim ps!!";
+        Scanner input=new Scanner(System.in);
+        System.out.print("Please give me a word!  ");
+        String word=input.nextLine();
+        //String word="This is some \\“really\\” great. (Text)!?”)";
         System.out.println(word.length());
-        ModuleProjectCrypto.normalizeText(word);
+        word=ModuleProjectCrypto.normalizeText(word);
+        ModuleProjectCrypto.Oboetics(word);
     }
 
     public static String normalizeText(String word){
@@ -16,18 +22,12 @@ public class ModuleProjectCrypto {
         for (int u=0;u<wordLength;u++){
 
             for (int i=0;i<wordLength;i++) {
-                word = word.replace('H', 'W');
                 char taht = word.charAt(i);
                 String otsitav = Character.toString(taht);
                 boolean isItLetter = Character.isLetter(taht);
-                System.out.println(isItLetter);
-
                 if (!isItLetter) {
                     firstPart = word.substring(0, i);
-                    System.out.println("i= " + i + " first word is " + firstPart);
                     secondPart = word.substring(i + 1);
-                    System.out.println("i= " + i + " second word is " + secondPart);
-                    // word=firstPart.concat(secondPart);
                     word = firstPart + secondPart;
                     wordLength = word.length();
                     System.out.println("word is currently: " + word);
@@ -36,6 +36,27 @@ public class ModuleProjectCrypto {
             }
         }
                     System.out.println(word);
+        return word;
+    }
+
+    public static String Oboetics(String word){
+        String firstPart, secondPart;
+        int wordLength=word.length();
+        for (int i=0; i<word.length();i++){
+            char taht=word.charAt(i);
+            if (taht=='O'||taht=='A'||taht=='E'||taht=='I'||taht=='Ü'||taht=='U'||taht=='Ä'||taht=='Ö'||taht=='Õ'){
+                firstPart=word.substring(0,i);
+                secondPart=word.substring(i,wordLength);
+                System.out.println("i= " + i + " first word is " + firstPart);
+                System.out.println("i= " + i + " second word is " + secondPart);
+                word=firstPart+"OB"+secondPart;
+                wordLength=word.length();
+                i=i+2;
+                System.out.println(word);
+
+            }
+        }
+        System.out.println(word);
         return word;
     }
 
