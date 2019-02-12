c980a543c56fc859a11781cdffa96a182afd6cc2
RRaa <35782666+RRaa@users.noreply.github.com>
2019-02-12 11:37:10 +0200

    Write example added

--- src/InputOutput/ReadBytes.java
+++ src/InputOutput/ReadBytes.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class ReadBytes {
     public static void main(String[] args)
     throws IOException{
-        byte data[]=new byte[6];
+       byte data[]=new byte[6];
         System.out.println("Enter smthng in max "+data.length+" char!");
         System.in.read(data);
         System.out.println("You wrote: ");
@@ -18,5 +18,15 @@ public class ReadBytes {
         System.out.println("Normal for-loop: ");
         for (int i=0;i< data.length;i++)
        System.out.println((char)data[i]);
+
+        System.out.println();
+
+        //example of write()
+        int a='X';
+        System.out.print("write: ");
+        System.out.write(a);
+        System.out.println();
+        System.out.println("println: "+a);
+
     }
 }
