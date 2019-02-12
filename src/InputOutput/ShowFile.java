e1e29e42a87cbcf4aaad79c0854467bb678d5b98
RRaa <35782666+RRaa@users.noreply.github.com>
2019-02-12 13:06:47 +0200

    Read from file; open in console

---
+++ src/InputOutput/ShowFile.java
@@ -0,0 +1,39 @@
+package InputOutput;
+import java.io.*;
+//java ShowFile FileForManipulation.txt;
+
+public class ShowFile {
+    public static void main(String[] args)
+    {
+        int i;
+        FileInputStream fin;
+
+        //is file specified:
+        if(args.length!=1){
+            System.out.println("Usage: ShowFile File");
+            return;
+        }
+
+
+        try{
+            //open the file
+            fin=new FileInputStream(args[0]);
+        }catch (FileNotFoundException exc){
+            System.out.println("File not found!");
+            return;
+        }
+
+        try{
+            //read bytes until EOF
+            do{
+                //read from file
+                i=fin.read();
+                if (i!= -1)System.out.print((char)i);
+                } while (i!=-1);
+        }catch (IOException exc){
+            System.out.println("Error reading file.");
+        }
+
+
+    }
+}
