94168f75fa6219666411082173c33b97449e3ffb
RRaa <35782666+RRaa@users.noreply.github.com>
2019-02-12 15:13:12 +0200

    Args value specified; Comments written to be more cleare

--- src/InputOutput/ShowFile.java
+++ src/InputOutput/ShowFile.java
@@ -5,42 +5,41 @@ import java.io.*;
 public class ShowFile {
     public static void main(String[] args)
     {
+        System.out.println("args.length = "+args.length );
         int i;
+
+        //make object from FilesInputStream Class
         FileInputStream fin;
 
-        //is file specified:
-        if(args.length!=1){
-            System.out.println("Usage: ShowFile File");
+        //check, if file is specified, it means that path is given:
+        if(args.length!=1){ //This code will be executed as soon as the program is called with parameters. it means: if address exists, then length =1
+            System.out.println("For using this program, please indicate a file as a parameter");
             return;
         }
 
-
+        //open the file
         try{
-            //open the file
             fin=new FileInputStream(args[0]);
         }catch (FileNotFoundException exc){
             System.out.println("File not found!");
             return;
         }
 
+        //read bytes until EOF
         try{
-            //read bytes until EOF
             do{
-                //read from file
-                i=fin.read();
+                i=fin.read(); //read from file
                 if (i!= -1)System.out.print((char)i);
                 } while (i!=-1);
         }catch (IOException exc){
             System.out.println("Error reading file.");
         }
 
+        //close the file
         try{
-            //close the file
             fin.close();
             } catch (IOException exc){
             System.out.println("Error closing file!");
         }
-
-
-    }
+        }
 }
