e02bd13340cb9136ced09d3fab4cf96a8b8091f7
RRaa <35782666+RRaa@users.noreply.github.com>
2019-02-15 15:55:45 +0200

    Compare two files

--- src/InputOutput/CompareFiles.java
+++ src/InputOutput/CompareFiles.java
@@ -1,4 +1,34 @@
 package InputOutput;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class CompareFiles {
+    public static void main(String[] args) {
+        int a, b;
+
+        if (args.length != 2) {
+            System.out.println("Please give files to compare!");
+            return;
+        }
+
+        try (FileInputStream fin1 = new FileInputStream(args[0]); FileInputStream fin2 = new FileInputStream(args[1])) {
+            a = fin1.read();
+            b = fin2.read();
+            if (a != -1 && b != -1) {
+                if (a != b) {
+                    System.out.println("files are different! In first file is written " + (char)a + " and in second file is written " + (char)b);
+                    return;
+                } else {
+                    System.out.println("Files are the same!");
+                }
+            } else {
+                System.out.println("One file is shorter than other!");
+                return;
+            }
+
+        } catch (IOException exc) {
+            System.out.println("IO error! " + exc);
+        }
+    }
 }
