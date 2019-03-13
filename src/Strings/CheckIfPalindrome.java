0ada78b9c4997c60ed050ea4723be128ead036eb
RRaa <35782666+RRaa@users.noreply.github.com>
2019-03-13 23:09:28 +0200

    Palindrome

--- src/Strings/CheckIfPalindrome.java
+++ src/Strings/CheckIfPalindrome.java
@@ -1,4 +1,16 @@
 package Strings;
 
 public class CheckIfPalindrome {
+    public static void main(String[] args) {
+        //word is palindrome, if it is same after reverse
+        String str = "Mee";
+        StringBuffer palindr = new StringBuffer(str);
+        palindr=palindr.reverse();
+        String palindr2 = new String(palindr);
+        if (str.equalsIgnoreCase(palindr2)) {
+            System.out.println("This word is a palindrome!");
+        } else {
+            System.out.println("This word is not a palindrome!");
+        }
+    }
 }
