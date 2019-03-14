25a0a665ae113f74c6f228b7a2aee5b98c4bd8ea
RRaa <35782666+RRaa@users.noreply.github.com>
2019-03-14 10:20:58 +0200

    Different String tasks

--- src/Strings/ReverseEachWord.java
+++ src/Strings/ReverseEachWord.java
@@ -1,4 +1,24 @@
 package Strings;
 
 public class ReverseEachWord {
+
+    public static void reverseWords(String input){
+        String[] splittedInput=input.split("\\s");
+        StringBuilder sb;
+
+        String output="";
+        int len=splittedInput.length;
+
+        for (int i=0;i<len;i++){
+            sb=new StringBuilder(splittedInput[i]);
+            sb=sb.reverse();
+            output+=sb.toString()+" ";
+            //System.out.println(sb);
+            }
+        System.out.println(output.trim());
+        }
+
+    public static void main(String[] args) {
+        ReverseEachWord.reverseWords("My little pony");
+    }
 }
