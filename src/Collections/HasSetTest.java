f79a4b2bca77cc960c0bb6ed15b9fe0a722d5cfd
RRaa <35782666+RRaa@users.noreply.github.com>
2019-03-15 23:21:03 +0200

    Collections

---
+++ src/Collections/HasSetTest.java
@@ -0,0 +1,41 @@
+package Collections;
+import java.util.HashMap;
+
+public class HasSetTest {
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCitites = new HashMap<String, String>();
+        HashMap<Integer, String> capitalNumbers = new HashMap<Integer, String>();
+        capitalCitites.put("Estonia", "Tallinn");
+        capitalCitites.put("Finland", "Helisinki");
+        System.out.println(capitalCitites);
+
+        for (String str : capitalCitites.keySet()) {
+            System.out.println(str);
+        }
+        for(String str:capitalCitites.values()){
+            System.out.println(str);
+        }
+
+        capitalNumbers.put(1, "Tallinn");
+        capitalNumbers.put(2, "Helisinki");
+        System.out.println(capitalNumbers);
+
+        for (int i : capitalNumbers.keySet()) {
+            System.out.println(i);
+        }
+        for(String str:capitalCitites.values()){
+            System.out.println(str);
+        }
+
+        capitalCitites.remove("Estonia");
+        for (String str : capitalCitites.keySet()) {
+            System.out.println(str);
+        }
+        for(String str:capitalCitites.values()){
+            System.out.println(str);
+        }
+        System.out.println();
+        String s1=capitalCitites.get("Finland");
+        System.out.println(s1);
+    }
+}
